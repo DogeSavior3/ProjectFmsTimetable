736034703ebc55e4a4f9b70a72a05a4db4d791fb
Michael Korol <2413902@gmail.com>
2022-12-24 23:20:49 +0700

    123

---
+++ testswiftlayout/123test/123test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  123test
+//
+//  Created by Михаил Король on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+}
+
